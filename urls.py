bd07dcf2cba64d4bb442a9e3356dc016f2618606
Shannon Mulloy <smulloy21@gmail.com>
2018-07-28 14:39:00 -0700

    send email with partner invite link

---
+++ urls.py
@@ -0,0 +1,28 @@
+"""code_for_good URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Pastebin API')
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('schema', schema_view),
+    path('send-invite', views.send_invite, name='send_invite'),
+    path('register', views.register, name='register'),
+]
--- views.py
+++ views.py
@@ -1,3 +1,36 @@
+import json
+
+from django.core.mail import send_mail
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.template.loader import get_template, render_to_string
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
+
+
+def index(request):
+    context = {}
+    return render(request, 'partner/index.html', context)
+
+
+@csrf_exempt
+def send_invite(request):
+    data = json.loads(request.body)
+    partner = data.get('partner', '')
+    # FIXME: should validate json data and send back an error if it fails validation
+    # TODO: partner db object should get saved at this point, with its id, email, and diaper_bank_id fields filled out
+    context = {'partner_id': partner['partner_id']}
+    html_content = get_template('partner/invite_partner.html').render(context)
+    plaintext_content = render_to_string('partner/invite_partner.txt', context)
+    send_mail('Diaper bank partner registration', plaintext_content, 'diaperbankpartners@gmail.com', [partner['email']], fail_silently=False, html_message=html_content)
+    return JsonResponse({'status': 'ok'})
+
+
+def register(request):
+    id = request.GET.get('id', '')
+    # FIXME: what should we return if there's no id?
+    # TODO: lookup partner by id and give them a form they can sumbit to their diaper bank
+    context = {}
+    return render(request, 'partner/register.html', context)
